ccb3332a7735a6733a6cccabe26435475429bd1f
jaepilchoi <jeffreyzzang@naver.com>
2019-10-01 16:21:45 +0900

    Add url attribute to projectpost model and temporarily replace project card's button to refer the url.

--- portfolio/project/models.py
+++ portfolio/project/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class ProjectPost(models.Model):
     title = models.CharField(max_length=140)
     content = models.TextField()
+    url = models.CharField(max_length=200, blank=True)
     document = models.FileField(upload_to='documents/', blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
