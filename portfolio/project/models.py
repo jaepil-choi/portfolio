29bcd2ec471e8aed22fae816aa131fb950e4d566
jaepil-choi <chljeffreyz@gmail.com>
2019-10-03 04:19:50 +0900

    Fix minor html/style for mobile screen.
    - Move home button into navbar to correctly display on mobile.
    - Wrap Django template language urls with "" to avoid bugs.
    - Set default="#" for ProjectPost model, to avoid crashing when there is no url specified for [open] button.
    - Migrate these changes.

--- portfolio/project/models.py
+++ portfolio/project/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class ProjectPost(models.Model):
     title = models.CharField(max_length=140)
     content = models.TextField()
-    url = models.CharField(max_length=200, blank=True)
+    url = models.CharField(max_length=200, blank=True, default='#')
     document = models.FileField(upload_to='documents/', blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
