ed7d7538ec67f58da09f2a16161fd1783d9d8fc9
jaepil-choi <chljeffreyz@gmail.com>
2019-10-03 02:44:24 +0900

    Separate home base navbar and sitewide base navbar.
    Set app_name in the app's urls.py and use URL namespacing in href.

--- portfolio/project/urls.py
+++ portfolio/project/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'project'
 urlpatterns = [
     path('', views.ProjectView.as_view(), name='list' ),
     path('<int:pk>', views.ProjectDetail.as_view(), name='detail' ),
